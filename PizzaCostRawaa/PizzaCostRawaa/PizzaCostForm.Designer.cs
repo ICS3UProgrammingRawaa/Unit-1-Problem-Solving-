3f01d616f014eac37a1b2189b12485b538ed86bc
Rawaa Al Ghanai <rawaa.alghanae@ocsbstudent.ca>
2020-03-02 11:50:44 -0500

    Update

---
+++ PizzaCostRawaa/PizzaCostRawaa/PizzaCostForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace PizzaCostRawaa
+{
+    partial class frmPizzaCost
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDiameter = new System.Windows.Forms.Label();
+            this.lblCost = new System.Windows.Forms.Label();
+            this.lblCostAnswer = new System.Windows.Forms.Label();
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDiameter
+            // 
+            this.lblDiameter.AutoSize = true;
+            this.lblDiameter.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiameter.Location = new System.Drawing.Point(64, 129);
+            this.lblDiameter.Name = "lblDiameter";
+            this.lblDiameter.Size = new System.Drawing.Size(365, 23);
+            this.lblDiameter.TabIndex = 0;
+            this.lblDiameter.Text = "Enter the diameter of the pizza (in inches):";
+            // 
+            // lblCost
+            // 
+            this.lblCost.AutoSize = true;
+            this.lblCost.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCost.Location = new System.Drawing.Point(64, 305);
+            this.lblCost.Name = "lblCost";
+            this.lblCost.Size = new System.Drawing.Size(228, 23);
+            this.lblCost.TabIndex = 1;
+            this.lblCost.Text = "The cost (including tax) is:";
+            // 
+            // lblCostAnswer
+            // 
+            this.lblCostAnswer.AutoSize = true;
+            this.lblCostAnswer.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCostAnswer.Location = new System.Drawing.Point(511, 305);
+            this.lblCostAnswer.Name = "lblCostAnswer";
+            this.lblCostAnswer.Size = new System.Drawing.Size(37, 23);
+            this.lblCostAnswer.TabIndex = 2;
+            this.lblCostAnswer.Text = "???";
+            // 
+            // txtDiameter
+            // 
+            this.txtDiameter.Location = new System.Drawing.Point(491, 132);
+            this.txtDiameter.Name = "txtDiameter";
+            this.txtDiameter.Size = new System.Drawing.Size(114, 20);
+            this.txtDiameter.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.White;
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(169, 202);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(177, 41);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate Cost";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // frmPizzaCost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Plum;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtDiameter);
+            this.Controls.Add(this.lblCostAnswer);
+            this.Controls.Add(this.lblCost);
+            this.Controls.Add(this.lblDiameter);
+            this.Name = "frmPizzaCost";
+            this.Text = "Pizza Cost By Rawaa";
+            this.Load += new System.EventHandler(this.FrmPizzaCost_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDiameter;
+        private System.Windows.Forms.Label lblCost;
+        private System.Windows.Forms.Label lblCostAnswer;
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ PizzaCostRawaa/PizzaCostRawaa/PizzaCostForm.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by: Rawaa Al Ghanai
+ * Created on: Feb. 28, 2020
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #12 - Pizza Cost 
+ * This program asks the user to enter the size of the pizza in inches. 
+ * Then given the following, it calculates the cost with 13% HST:
+ *      labour cost is $0.75 / pizza, regardless of the size
+ *      rent of the shop is $1.00 / pizza, regardless of the size
+ *      materials is $0.50 / diameter inch of pizza.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaCostRawaa
+{
+    public partial class frmPizzaCost : Form
+    {
+        public frmPizzaCost()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            double diameter, costBeforeTax, costAfterTax;
+
+            // convert diameter to double
+            diameter = double.Parse(txtDiameter.Text);
+
+            // calculate the cost before and after the tax
+            costBeforeTax = 1.75 + 0.5 * diameter;
+            costAfterTax = 1.13 * costBeforeTax;
+
+            // display the cost in the label
+            lblCostAnswer.Text = String.Format("${0:0.00}", costAfterTax);
+
+            // show the cost and cost answer labels
+            this.lblCost.Show();
+            this.lblCostAnswer.Show();
+
+        }
+
+        private void FrmPizzaCost_Load(object sender, EventArgs e)
+        {
+            // hide the cost and cost answer labels.
+            this.lblCost.Hide();
+            this.lblCostAnswer.Hide();
+        }
+    }
+}
